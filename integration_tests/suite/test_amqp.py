056d29b851d850def9ae57bf82e9d1a6b256e9b8
Nicolaos Ballas <nballas@wazo.io>
2019-07-09 07:43:40 -0400

    added test for amqp events

---
+++ integration_tests/suite/test_amqp.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017-2019 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+import ari as ari_client
+import logging
+import os
+import pytest
+
+from hamcrest import assert_that
+from hamcrest import calling
+from hamcrest import has_property
+from requests.exceptions import HTTPError
+from xivo_test_helpers import until
+from xivo_test_helpers.asset_launching_test_case import AssetLaunchingTestCase
+from xivo_test_helpers.hamcrest.raises import raises
+
+log_level = logging.DEBUG if os.environ.get('TEST_LOGS') == 'verbose' else logging.INFO
+logging.basicConfig(level=log_level)
+
+
+class AssetLauncher(AssetLaunchingTestCase):
+
+    assets_root = os.path.join(os.path.dirname(__file__), '..', 'assets')
+    asset = 'base'
+    service = 'asterisk'
+
+
+@pytest.fixture()
+def ari():
+    AssetLauncher.kill_containers()
+    AssetLauncher.rm_containers()
+    AssetLauncher.launch_service_with_asset()
+    ari_url = 'http://localhost:{port}'.format(port=AssetLauncher.service_port(5039, 'ari'))
+    ari = until.return_(ari_client.connect, ari_url, 'wazo', 'wazo', timeout=5, interval=0.1)
+
+    yield ari
+
+    AssetLauncher.kill_containers()
+
+# what??!
+def test_stasis_amqp_events(ari):
+    res = calling(ari.wazo.createChannel).with_args()
+    expected = raises(HTTPError).matching(has_property('response', has_property('status_code', 204)))  # 200 ???
+    assert_that(res, expected)
