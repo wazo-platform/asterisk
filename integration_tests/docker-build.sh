e110eb82948cd783936003ab58b78368fc6ffdd6
Francois Blackburn <fblackburn@wazo.community>
2020-10-08 13:29:53 -0400

    sed s/wazopbx/wazoplatform/

--- integration_tests/docker-build.sh
+++ integration_tests/docker-build.sh
@@ -12,7 +12,7 @@ if [ -f ${PKGDIR}/Packages ]; then
     rm -f Packages *.deb
     ln -f ${PKGDIR}/{Packages,*.deb} . || ln -sf ${PKGDIR}/{Packages,*.deb} .
     sed -e "s@RUN echo@COPY Packages *.deb /var/tmp/\nRUN echo \"deb [trusted=yes] file:///var/tmp ./\" > /etc/apt/sources.list.d/wazo-dev.list \&\& echo@" < ../Dockerfile > Dockerfile
-    docker build -t wazopbx/asterisk .
+    docker build -t wazoplatform/asterisk .
     rm -f Dockerfile Packages *.deb
 fi
 
